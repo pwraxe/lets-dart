137a606b686cc4b57e357992b36371dd55d31de6
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-01-31 14:22:25 +0530

    syntax of if_else condition
    ignorable file...

---
+++ if_else_syntax.dart
@@ -0,0 +1,64 @@
+/*
+void main(){
+
+  ----------case 1 :-----------
+
+    if(condition){
+      ..statement..
+    }
+
+  ----------case 2 :-----------
+
+  if(condition){
+    ..statement..
+  }else{
+    ..statement..
+  }
+
+  ----------case 3 :-----------
+
+  if(condition){
+    ..statement..
+  }else if(condition){
+    ..statement..
+  }else if(condition){
+    ..statement..
+  }else{
+    ..statement..
+  }
+
+  ----------case 4 :-----------
+
+  if(condition){
+    if(condition){
+        if(condition){
+          multiple nested if(condition)
+        }else{
+          if(condition){
+            multiple nested if(condition)
+          }else{
+            multiple nested if(..) else conditions
+        }
+      }
+    }else{}
+  }else{}
+
+  ----------case 5 :-----------
+
+  if(condition1 && condition2 ) {
+    if(condition1 || condition2 ) {
+       if( !condition ){}
+    }
+  }
+}
+
+-----------------operator used in conditions
+
+==  ==>  (a == b)
+!=  ==>  (a != b)
+>=  ==>  (a >= b)
+<=  ==>  (a <= b)
+
+and more...
+
+*/

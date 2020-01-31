83a483d2928b52577959de8bb098981b33e66245
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-01-31 10:43:54 +0530

    Update customGetter&Setter.dart

--- customGetter&Setter.dart
+++ customGetter&Setter.dart
@@ -28,3 +28,33 @@ void main()
   print('Name : ${s.s_name}');  //custom getter
 
 }
+
+============================================getter & Setter Function name can be different=============================
+    
+class Student
+{
+  int id;
+  String name,address;
+
+  set s_id(int id){ this.id = id; }                 // setter
+  set s_name(String name){ this.name = name; }      // setter
+  set s_addr(String addr){ address = addr;  }       // setter
+
+  int get g_id => id;                               // getter
+  String get g_name => name;                        // getter
+  String get g_addr => address;                     // getter
+}
+
+
+void main(){
+
+  var s = Student();
+    
+  s.s_id = 7;                                       // setting values       
+  s.s_name = 'Akshay Pawar';                        // setting values
+  s.s_addr = 'Shivaji Nagar Sinnar';                // setting values
+
+  print('ID : ${s.g_id}');                          // getting values
+  print('Name : ${s.g_name}');                      // getting values
+  print('Address : ${s.g_addr}');                   // getting values
+}
